b91158b0faede6e20ce73c91492989e571b22699
Sean Outhwaite <sean.outhwaite@gmail.com>
2026-03-02 20:18:22 +1300

    Setup firebase auth

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,56 @@
 import { Image } from 'expo-image'
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet } from 'react-native'
 
 import { HelloWave } from '@/components/hello-wave'
 import ListView from '@/components/list-view'
 import { ThemedText } from '@/components/themed-text'
 import { ThemedView } from '@/components/themed-view'
+import {
+  FirebaseAuthTypes,
+  getAuth,
+  onAuthStateChanged,
+  signInAnonymously,
+} from '@react-native-firebase/auth'
 import { Link } from 'expo-router'
+import { useEffect, useState } from 'react'
 
 export default function HomeScreen() {
+  const [initializing, setInitializing] = useState(true)
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null)
+
+  // Handle user state changes
+  function handleAuthStateChanged(user: FirebaseAuthTypes.User | null) {
+    setUser(user)
+    if (initializing) setInitializing(false)
+  }
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), handleAuthStateChanged)
+    return subscriber // unsubscribe on unmount
+  }, [])
+
+  if (initializing) return null
+
+  if (!user) {
+    return (
+      <ListView
+        headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+        headerImage={
+          <Image
+            source={require('@/assets/images/partial-react-logo.png')}
+            style={styles.reactLogo}
+          />
+        }
+      >
+        <ThemedView style={styles.titleContainer}>
+          <Pressable onPress={() => signInAnonymously(getAuth())}>
+            <ThemedText>Login</ThemedText>
+          </Pressable>
+        </ThemedView>
+      </ListView>
+    )
+  }
+
   return (
     <ListView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -24,7 +67,7 @@ export default function HomeScreen() {
       </ThemedView>
 
       <ThemedView>
-        <ThemedText type="subtitle">This will be login screen</ThemedText>
+        <ThemedText type="subtitle">Loggged in as {user.email}</ThemedText>
         <ThemedText type="link">
           <Link href="/(tabs)">Home</Link>
         </ThemedText>
